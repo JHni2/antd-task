2f79e20db7eb791add84608b5ad66115e9c3e3d3
JHni2 <wlgus0260@naver.com>
2023-11-24 13:20:53 +0900

    [feat] 칼럼 양 옆으로 늘리기

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ const App = () => {
       date: '2018-02-11',
       amount: 120,
       type: 'income',
-      note: 'transfer',
+      note: 'transfertransfertransfertransfertransfertransfertransfertransfer',
       address: 'London Park no. 0',
       status:'Finished',
       upgradeStatus:'Upgraded: 56',
@@ -192,10 +192,29 @@ const App = () => {
 
       // 칼럼을 오른쪽으로 드래그 했을 떄
       if (isResizingRight) {
-        // 더이상 오른쪽으로 갈 수 X
-        if(newColumns[index + 1].width <= minColWidth){
-          return prevColumns
+        // 더이상 오른쪽으로 갈 수 없어 오른쪽의 칼럼을 줄이는 동작을 수행
+        let currentIndex = index + 1;
+
+        while (currentIndex < newColumns.length && newColumns[currentIndex].width <= minColWidth) {
+          newColumns[currentIndex] = {
+            ...newColumns[currentIndex],
+            width: minColWidth,
+          };
+
+          // 가장 오른쪽 칼럼의 가로 너비가 최소값을 갖도록 함
+          if(currentIndex + 1 === newColumns.length){
+            return prevColumns
+          }
+      
+          if (currentIndex + 1 < newColumns.length) {
+            newColumns[currentIndex + 1] = {
+              ...newColumns[currentIndex + 1],
+              width: newColumns[currentIndex + 1].width + (prevtWidth - newWidth),
+            };
+          }
+          currentIndex++;
         }
+        
         // 더이상 오른쪽으로 갈 수 O
           newColumns[index + 1] = {
             ...newColumns[index + 1],
@@ -205,14 +224,39 @@ const App = () => {
 
       // 칼럼을 왼쪽으로 드래그 했을 떄
       } else if (isResizingLeft) {
-        newColumns[index + 1] = {
-          ...newColumns[index + 1],
-          width: newColumns[index + 1].width + (prevtWidth - newWidth),
-        };
+        // 더이상 왼쪽으로 갈 수 없어 왼쪽의 칼럼을 줄이는 동작을 수행
+        let currentIndex = index;
+
+        while (currentIndex < newColumns.length && newColumns[currentIndex].width <= minColWidth) {
+          newColumns[currentIndex] = {
+            ...newColumns[currentIndex],
+            width: minColWidth,
+          };
+          
+          // 가장 왼쪽 칼럼의 가로 너비가 최소값을 갖도록 함
+          if(currentIndex===0){
+            return prevColumns
+          }
+
+          if (currentIndex - 1 < newColumns.length) {
+            newColumns[currentIndex - 1] = {
+              ...newColumns[currentIndex - 1],
+              width: newColumns[currentIndex - 1].width - (prevtWidth - newWidth),
+            };
+          }
+
+          currentIndex--;
+        }
+        
+        // 더이상 왼쪽으로 갈 수 O
+          newColumns[index + 1] = {
+            ...newColumns[index + 1],
+            width: newColumns[index + 1].width + (prevtWidth - newWidth),
+          };
+          return newColumns
+        }
+        
         return newColumns
-      }
-      
-      return newColumns;
     });
   };
 
